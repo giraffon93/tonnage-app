e59d185cc0629cc671e1cf3d6ab20f1f4d920529
giraffon93 <monjet.jeremy@hotmail.com>
2026-06-01 22:33:08 +0200

    Update sw

--- sw.js
+++ sw.js
@@ -19,4 +19,17 @@ self.addEventListener("activate", event => {
       Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
-         
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
