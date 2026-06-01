a4996c80deb34095b3827cceaec2282b5f8be450
giraffon93 <monjet.jeremy@hotmail.com>
2026-06-01 20:31:17 +0200

    Update service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tonnage-v1";
+const CACHE_NAME = "tonnage-v2";
 
 const FILES = [
   "./",
@@ -6,14 +6,37 @@ const FILES = [
   "./manifest.json"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
+// INSTALL
+self.addEventListener("install", (event) => {
+  self.skipWaiting(); // force activation immédiate
+
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim(); // prend le contrôle direct
+});
+
+// FETCH
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
   );
 });
