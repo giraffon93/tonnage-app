33649c54f3d512f2d268c83c6ecf2113595e41c7
giraffon93 <monjet.jeremy@hotmail.com>
2026-06-01 20:28:17 +0200

    Update service-worker

--- service-worker.js
+++ service-worker.js
@@ -6,13 +6,13 @@ const FILES = [
   "./manifest.json"
 ];
 
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
